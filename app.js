19adcb8a05042ea46b74641688de606c8f19d548
Paul-Kl01 <kluge.paul@gmx.de>
2020-11-27 20:51:26 +0100

    AddScore() und add() hinzugefügt

--- app.js
+++ app.js
@@ -12,69 +12,51 @@ class Person {
     getName() {
         return this.name;
     }
+
+    addScore() {
+        let punkte = 0;
+        punkte = parseInt(document.getElementById(this.name).value);
+        if (document.getElementById(this.name).value != "") {
+            this.score = this.score + punkte;
+            document.getElementById(this.name + "P").innerHTML = this.score;
+            document.getElementById(this.name).value = "";
+            console.log(this.getName(),":", punkte);
+        }
+    }
 }
 
-// Objekt erstellen
+// Nutzer erstellen
 let jul = new Person("julian", 0);
 let han = new Person("hannah", 0);
 let lea = new Person("lea", 0);
 let pau = new Person("paul", 0);
 
-let punkte = 0;
-
 // Punkte hinzufügen
-function addJulian() {
-    punkte = parseInt(document.getElementById("julian").value);
+function add() {
     if (document.getElementById("julian").value != "") {
-        jul.score = jul.score + punkte;
-        document.getElementById("jPunkte").innerHTML = jul.score;
-        document.getElementById("julian").value = "";
-        console.log(jul.getName(),":", punkte);
+        jul.addScore();
+    } else if (document.getElementById("hannah").value != "") {
+        han.addScore();
+    } else if (document.getElementById("lea").value != "") {
+        lea.addScore();
+    } else {
+        pau.addScore();
     }
 }
 
-function addHannah() {
-    punkte = parseInt(document.getElementById("hannah").value);
-    if (document.getElementById("hannah").value != "") {
-        han.score = han.score + punkte;
-        document.getElementById("hPunkte").innerHTML = han.score;
-        document.getElementById("hannah").value = "";
-        console.log(han.getName(),":", punkte);
-    }
-}
-
-function addLea() {
-    punkte = parseInt(document.getElementById("lea").value);
-    if (document.getElementById("lea").value != "") {
-        lea.score = lea.score + punkte;
-        document.getElementById("lPunkte").innerHTML = lea.score;
-        document.getElementById("lea").value = "";
-        console.log(lea.getName(),":", punkte);
-    }
-}
-
-function addPaul() {
-    punkte = parseInt(document.getElementById("paul").value);
-    if (document.getElementById("paul").value != "") {
-        pau.score = pau.score + punkte;
-        document.getElementById("pPunkte").innerHTML = pau.score;
-        document.getElementById("paul").value = "";
-        console.log(pau.getName(),":", punkte);
-    }
-}
-
-// Punkte aktualisieren
-function aktualisiere() {
-    document.getElementById("jPunkte").innerHTML = jul.getPunkte();
-    document.getElementById("hPunkte").innerHTML = han.getPunkte();
-    document.getElementById("lPunkte").innerHTML = lea.getPunkte();
-    document.getElementById("pPunkte").innerHTML = pau.getPunkte();
+// Punkte reset
+function reset() {
+    document.getElementById("julianP").innerHTML = jul.getPunkte();
+    document.getElementById("hannahP").innerHTML = han.getPunkte();
+    document.getElementById("leaP").innerHTML = lea.getPunkte();
+    document.getElementById("paulP").innerHTML = pau.getPunkte();
 }
 
 // Rangliste sortieren
+/*
 let rangliste = [jul.score];
 console.log(rangliste);
 function sort() {
 
 }
-
+*/
